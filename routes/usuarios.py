46819822a77406059600b3289b5c3ea86d7db876
Esneider Roquemes <esneiderlastre@gmail.com>
2024-11-05 17:27:30 -0500

    Cambios en register

--- routes/usuarios.py
+++ routes/usuarios.py
@@ -2,7 +2,12 @@ from services.user_queries import User_queries
 from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify
 from flask_login import login_user, logout_user, current_user
 from models.usuarios import Usuarios
+import re
 usuarios = Blueprint('usuarios', __name__)
+
+def validar_contraseña(password):
+    regex = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$'
+    return bool(re.match(regex, password))
 
 @usuarios.route('/register', methods = ['POST', 'GET'])
 def registrar_usuarios():
@@ -15,6 +20,9 @@ def registrar_usuarios():
         fk_rol = 3
         if not username or not password:
             return redirect(url_for('usuarios.registrar_usuarios'))
+        
+        if not validar_contraseña(password):
+            return render_template('register.html', error = 'La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una minúscula, un número y un carácter especial')
         
         User_queries.registrar_usuarios(nombres, apellidos, username, telefono, password, fk_rol)
         return redirect(url_for('usuarios.login'))
